15e8acd4a3b6a783085fa8ea0ceb3df584599613
LucasE2996 <moeda.60@gmail.com>
2018-04-03 18:46:25 -0300

    update

--- teste.js
+++ teste.js
@@ -1,25 +1,32 @@
+// var matriz = [
+//   [-2, -1, -1, -1, -3],
+//   [-1, -1, -1, -3, -1],
+//   [-2, -1, -1, -1, -1],
+//   [-1, -1, -1, -1, -1],
+//   [-1, -1, -1, -1, -1]
+// ];
+
 var matriz = [
-  [1, 2, 1, 1, 1, 3],
-  [1, 1, 1, 1, 3, 1],
-  [1, 2, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 2, 1, 2]
+  [2, 1, 1, 1, 3],
+  [1, 1, 1, 3, 1],
+  [2, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1]
 ];
 
 function calc() {
-  var maior = 0;
+  var maior = -999999;
   var produtoLinha;
   var produtoColuna;
   var produtoDiagonalEsq;
   var produtoDiagonalDir;
-    for (var i =0; i < matriz.length; i++) {0
+    for (var i =0; i < matriz.length; i++) {
       for(var j =0; j < matriz.length; j ++) {
-        produtoLinha = 1;
-        produtoColuna = 1;
-        produtoDiagonalEsq = 1;
-        produtoDiagonalDir = 1;
-        for(var k = 4; k >= 0; k--) {
+        produtoLinha = matriz[i][j];
+        produtoColuna = matriz[i][j];
+        produtoDiagonalEsq = matriz[i][j];
+        produtoDiagonalDir = matriz[i][j];
+        for(var k = 4; k > 0; k--) {
           if (matriz[i][j+4])
             produtoLinha *= matriz[i][j+k];
           if (matriz[i+4])
@@ -28,6 +35,7 @@ function calc() {
             produtoDiagonalDir *= matriz[i+k][j+k];
           if (matriz[i+4] && matriz[i+4][j-4])
             produtoDiagonalEsq *= matriz[i+k][j-k];
+          debugger;
         }
       if ( produtoLinha > maior) {
         maior = produtoLinha;
